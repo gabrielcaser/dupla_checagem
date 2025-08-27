2f1a707c98aa0e01ac067c1c3b0c184df6db8638
gabrielcaser <gabrielcaser9@hotmail.com>
2025-08-27 16:47:44 -0300

    renv

--- codigo/00-main.r
+++ codigo/00-main.r
@@ -1,3 +1,5 @@
 # Descrição - Esse programa é o script principal que roda os demais da dupla-checagem de elegibilidade do Pé de Meia
 
-# 
+# Definindo o ambiente renv
+install.packages("renv")
+renv::init()
